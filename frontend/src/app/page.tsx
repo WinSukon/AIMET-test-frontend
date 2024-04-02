d8ff26ea7c9c3d20ad5a6a396b1c91d214fba6ce
WinSukon <jakrinsukon@gmail.com>
2024-04-02 19:53:24 +0700

    feat: temp change front when got wanted data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,18 +11,12 @@ export default function Home() {
   const id = useRef<string>("");
   const status = useRef<string>("");
   const levels = useRef<string>("");
-  // const [status, setStatus] = useState<string>("");
-  // const [levels, setLevels] = useState<string>("");
+
+  const [show, setShow] = useState<boolean>(false);
 
   const AIresult = async (id: string) => {
     const res = await getResult(id);
     if (res) {
-      console.log(
-        id,
-        res.message,
-        res.data.result_ai_status,
-        res.data.result_ai_severity_level
-      );
       status.current = res.data.result_ai_status;
 
       if (res.data.result_ai_severity_level !== null) {
@@ -49,7 +43,8 @@ export default function Home() {
       AIresult(id.current);
       if (status.current === "FINISHED") {
         clearInterval(interval);
-        console.log("interval cleared")
+        setShow(true);
+        console.log("interval cleared");
       }
     }, 1000);
   }, [id]);
@@ -61,7 +56,9 @@ export default function Home() {
           <SeverityResult value={100} />
         </div>
       </Suspense> */}
-      {levels.current!=="" ? <SeverityResult value={100} /> : <Loading></Loading>}
+      <div className="flex justify-center  w-full">
+        {show ? <SeverityResult value={100} /> : <Loading></Loading>}
+      </div>
     </main>
   );
 }
